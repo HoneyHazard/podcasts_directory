6867a8f69f13051c6e00aacfbdfd112c61dc8424
HoneyHazard <8847050+HoneyHazard@users.noreply.github.com>
2023-06-17 01:21:59 -0700

    updatge

--- play_science_tech_history_etc_local.sh
+++ play_science_tech_history_etc_local.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-for f in `cat science_tech_history_etc.m3u | shuf` ; do
+playlist_filename="temp_playlist.m3u"
+#player_command="smplayer ${playlist_filename}"
+#player_command="nvlc --loop --random --playlist-autostart ${playlist_filename}"
+#player_command="vlc --loop --random --playlist-autostart ${playlist_filename}"
+player_command="celluloid ${playlist_filename}"
+
+rm ${playlist_filename}
+
+for f in `cat science_tech_history_etc.m3u` ; do
+#for f in `cat science_tech_history_etc.m3u | head -n 3` ; do
     echo ============= ${f} ================
 
     for url in `curl --silent ${f} | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*(mp3|mp4|m4a|ogg)" | uniq | head -n 2`; do
        echo ${url}
-       smplayer -add-to-playlist ${url} > /dev/null
+       #smplayer -add-to-playlist ${url} > /dev/null
+       echo ${url} >> ${playlist_filename}
     done
-    sleep 5
+#    sleep 5
 done
+
+${player_command} &
